11b25e6a4df536314de90c849726931b3f45eed2
Ckey230 <Ckey230@gmail.com>
2023-01-12 18:58:08 -0500

    artistID

--- src/Components/ArtistView.js
+++ src/Components/ArtistView.js
@@ -2,25 +2,37 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 function ArtistView() {
-    const { artistId } = useParams()
-    const [ artistData, setArtistData ] = useState([])
+    const { id } = useParams()
+    const [ artistData, setArtistData ] = useState([]) //165907
+    // const albums = artistData.filter(entry => entry.CollectionType === 'Album')
+    const albumDisplay = artistData.map(album => {
+        return (
+            <div key={album.collectionId}>
+                <p>{album.collectionName}</p>
+            </div>
+        )
+    })
 
     useEffect(() => {
-        const API_URL = `http://localhost:4000/album/${artistId}`
+        const API_URL = `http://localhost:4000/album/${id}`
         const fetchData = async () => {
             const response = await fetch(API_URL)
             const resData = await response.json()
             console.log(resData)
+            const albums = resData.results.filter(entry => entry.collectionType === 'Album')
+            setArtistData(albums)
         }
         fetchData()
-    })
-    
+    }, [id])
+
     return (
         <div>
-            <h1>id: {artistId}</h1>
+            <h1>id: {id}</h1>
+            {albumDisplay}
         </div>
     )
 }
+
 
 export default ArtistView
 
